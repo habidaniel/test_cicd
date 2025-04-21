444fa44df019a2baff10d3066d56fe7d5eba7f9c
habidaniel <habimana2daniel@gmail.com>
2025-04-21 16:17:54 +0200

    index changes

--- patient/src/index.js
+++ patient/src/index.js
@@ -5,4 +5,4 @@ document.body.innerHTML = '<div id="app"></div>';
 
 // Render your React component instead
 const root = createRoot(document.getElementById('app'));
-root.render(<h1>Hello, world</h1>);
+root.render(<h1> world</h1>);

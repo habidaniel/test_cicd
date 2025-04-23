3d458eca4b5a20d67240f94666fb97d6c3eed15d
habidaniel <habimana2daniel@gmail.com>
2025-04-23 10:33:02 +0200

    correct data

--- patient/src/index.js
+++ patient/src/index.js
@@ -5,4 +5,4 @@ document.body.innerHTML = '<div id="app"></div>';
 
 // Render your React component instead
 const root = createRoot(document.getElementById('app'));
-root.render(<h1>daniel</h1>);
+root.render(<h1>daniel habimana</h1>);
